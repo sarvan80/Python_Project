161c1f097b118580bf68a4cb42c0bfb65e7e5b06
Saravanan <thiagarajan.saravanan@gmail.com>
2017-05-21 22:20:40 -0400

    no message

---
+++ Tools.py
@@ -0,0 +1,13 @@
+# this is a comment
+#
+# Author : Lars
+#
+# library for palindrome prog
+
+def isPal(thestr):
+    if thestr == thestr[::-1]:
+        return True
+    else:
+        return False
+
+Hodges = 14
